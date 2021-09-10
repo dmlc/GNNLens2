b3d355f01750ca7bf202840e50365e7851f1342c
mufeili <mufeili1996@gmail.com>
2021-09-10 14:27:47 +0800

    Update

--- gnnvis/mini_server/visbuild/precache-manifest.eb1441d966044d52a67c13459b50684f.js
+++ gnnvis/mini_server/visbuild/precache-manifest.eb1441d966044d52a67c13459b50684f.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cc7ea11f0c39611298af2e098e342aae",
+    "revision": "956faaef47d9e74d90ee08bf627e18a2",
     "url": "/index.html"
   },
   {
-    "revision": "4833c410bc5213b50c67",
+    "revision": "695f4ae6bc8a26fecc38",
     "url": "/static/css/2.929389fa.chunk.css"
   },
   {
-    "revision": "9bd190fcde8b604a2e47",
+    "revision": "8ab7c75cead15d2ca8c0",
     "url": "/static/css/main.8d5084fd.chunk.css"
   },
   {
-    "revision": "4833c410bc5213b50c67",
-    "url": "/static/js/2.033d9361.chunk.js"
+    "revision": "695f4ae6bc8a26fecc38",
+    "url": "/static/js/2.87ad82ac.chunk.js"
   },
   {
     "revision": "ca9e56bc4f9848094e5b4e249ca2f146",
-    "url": "/static/js/2.033d9361.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.87ad82ac.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9bd190fcde8b604a2e47",
-    "url": "/static/js/main.d4ab5d39.chunk.js"
+    "revision": "8ab7c75cead15d2ca8c0",
+    "url": "/static/js/main.80849a8d.chunk.js"
   },
   {
     "revision": "bda4a6a5a35ea013f007",
