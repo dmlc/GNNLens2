75e5a8d15358f6b451e799262c6aeaf981c5fab7
Mufei Li <mufeili1996@gmail.com>
2021-09-10 14:45:56 +0800

    Upgrade axios (#3)
    * Update
    
    * Update
    
    * Update
    
    * Delete 2.033d9361.chunk.js
    
    * Delete 2.033d9361.chunk.js.map
    
    * Rename 2.033d9361.chunk.js.LICENSE.txt to 2.87ad82ac.chunk.js.LICENSE.txt
    
    * Add files via upload
    
    * Add files via upload
    
    * Update
    
    * Delete main.d4ab5d39.chunk.js
    
    * Delete main.d4ab5d39.chunk.js.map
    
    * Delete main.e33a8010.chunk.js
    
    * Delete main.e33a8010.chunk.js.map
    
    * Add files via upload

---
+++ gnnvis/mini_server/visbuild/precache-manifest.eb1441d966044d52a67c13459b50684f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "956faaef47d9e74d90ee08bf627e18a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "695f4ae6bc8a26fecc38",
+    "url": "/static/css/2.929389fa.chunk.css"
+  },
+  {
+    "revision": "8ab7c75cead15d2ca8c0",
+    "url": "/static/css/main.8d5084fd.chunk.css"
+  },
+  {
+    "revision": "695f4ae6bc8a26fecc38",
+    "url": "/static/js/2.87ad82ac.chunk.js"
+  },
+  {
+    "revision": "ca9e56bc4f9848094e5b4e249ca2f146",
+    "url": "/static/js/2.87ad82ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ab7c75cead15d2ca8c0",
+    "url": "/static/js/main.80849a8d.chunk.js"
+  },
+  {
+    "revision": "bda4a6a5a35ea013f007",
+    "url": "/static/js/runtime-main.f5679f09.js"
+  },
+  {
+    "revision": "4b8ec5cf962ffb622f6d7c0a1a8f8385",
+    "url": "/static/media/logo2.4b8ec5cf.png"
+  }
+]);
