1579b55bdbb85cf0da9b6a7dbfe8b30fe9813e2f
Mufei Li <mufeili1996@gmail.com>
2021-10-06 14:12:47 +0800

    Fix NLabel Switch in Custom Subgraph Visualization (#8)
    * Update
    
    * Remove redundant files
    
    * Remove extra data files
    
    * License header
    
    * Update
    
    * Fix NLabel switch in custom subgraph visualization

---
+++ python/gnnlens/visbuild/precache-manifest.61eeda3f5dd1495de4355615963b0c0f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de37e4e7fb95e71314f42048ef5bb255",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df959a132ebb6c1acbb3",
+    "url": "/static/css/2.929389fa.chunk.css"
+  },
+  {
+    "revision": "e7066fbd30295823adf7",
+    "url": "/static/css/main.8d5084fd.chunk.css"
+  },
+  {
+    "revision": "df959a132ebb6c1acbb3",
+    "url": "/static/js/2.b9f0e663.chunk.js"
+  },
+  {
+    "revision": "ca9e56bc4f9848094e5b4e249ca2f146",
+    "url": "/static/js/2.b9f0e663.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7066fbd30295823adf7",
+    "url": "/static/js/main.e553e190.chunk.js"
+  },
+  {
+    "revision": "2d9282ac3e4bee87b5f2",
+    "url": "/static/js/runtime-main.ce081a3c.js"
+  },
+  {
+    "revision": "4b8ec5cf962ffb622f6d7c0a1a8f8385",
+    "url": "/static/media/logo2.4b8ec5cf.png"
+  }
+]);
